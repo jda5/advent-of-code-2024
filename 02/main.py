370177ac48c2f100ca6208508849b556580c7748
Jacob Strauss <jacob.d.strauss@gmail.com>
2024-12-03 09:26:33 +0000

    Puzzle two: solution two

--- 02/main.py
+++ 02/main.py
@@ -5,11 +5,11 @@ reports = load_input('02')
 
 def is_safe(a: int, b: int, is_increasing: bool) -> bool:
     diff = b - a
-    if is_increasing ^ (diff > 0) or not (1 <= abs(diff) <= 3):
+    if (is_increasing ^ (diff > 0)) or not (1 <= abs(diff) <= 3):
         return False    
     return True
 
-def is_safe_report(report: tuple[int]) -> int:
+def is_safe_report(report: list[int]) -> int:
     prev = report[0]
     is_increasing = prev < report[1]
     for curr in report[1:]:
@@ -26,17 +26,39 @@ def puzzle_one() -> int:
     num_safe = 0
 
     for report in reports:
-        report = tuple(map(int, report.split()))
+        report = list(map(int, report.split()))
         num_safe += is_safe_report(report)
 
     return num_safe
+
+def puzzle_two() -> int:
+    # the same rules apply as before, except if removing a single level from an 
+    # unsafe report would make it safe, the report instead counts as safe.
+    num_unsafe = 0
+
+    for report in reports:
+        report = list(map(int, report.split()))
+        prev_i, prev = 0, report[0]
+        is_increasing = prev < report[1]
+        for curr_i, curr in enumerate(report[1:], 1):
+            if not is_safe(prev, curr, is_increasing):
+
+                report_with_curr_removed = report[:curr_i] + report[curr_i + 1:]
+                report_with_prev_removed = report[:prev_i] + report[prev_i + 1:]
+                
+                if not (is_safe_report(report_with_curr_removed) or is_safe_report(report_with_prev_removed)):
+                    num_unsafe += 1
+                break
+
+            prev, prev_i = curr, curr_i
+
+    return len(reports) - num_unsafe
 
 
     
 
 # def puzzle_two() -> int:
-#     # the same rules apply as before, except if removing a single level from an 
-#     # unsafe report would make it safe, the report instead counts as safe.
+
 #     num_unsafe = 0
 
 #     for report in reports:
@@ -99,4 +121,4 @@ def puzzle_one() -> int:
 
 if __name__ == "__main__":
     print(puzzle_one())
-    # print(puzzle_two())
+    print(puzzle_two())
